35e8099e409659906c8d86c480ee2585981483f0
danlaanh202 <mushroomlazy259@gmail.com>
2023-03-23 21:10:09 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
+// StyleSheet is the same as styledcomponent, Text means <p></p> , View is the same as <div></div> but non-scrolling
 export default function App() {
+  const [input, setInput] = useState("");
+  const [goals, setGoals] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const handleShowModal = () => {
+    setIsModalVisible(true);
+  };
+  const goalInputHandler = (enteredText) => {
+    setInput(enteredText);
+  };
+  const addGoalHandler = () => {
+    setGoals((prev) => [
+      {
+        text: input,
+        id: Math.random().toString(),
+      },
+      ...prev,
+    ]);
+    setInput("");
+    setIsModalVisible(false);
+  };
+  const deleteGoal = (index) => {
+    setGoals((prev) => {
+      return prev.filter((elem) => elem.id !== index);
+    });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Button title="Nhấp vào để thêm mục đích" onPress={handleShowModal} />
+      <GoalInput
+        isModalVisible={isModalVisible}
+        input={input}
+        onChangeText={goalInputHandler}
+        onPress={addGoalHandler}
+      />
+
+      <View style={styles.goalListContainer}>
+        <Text style={{ paddingHorizontal: 8 }}>Danh sách mục đích:</Text>
+        <FlatList
+          alwaysBounceVertical={false}
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                deleteGoal={() => {
+                  deleteGoal(itemData.item.id);
+                }}
+                itemData={itemData}
+                key={itemData.item.id}
+              />
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  goalScrollView: {
+    paddingHorizontal: 8,
+  },
+  goalListContainer: {
+    flex: 1,
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+const GoalItem = ({ itemData, deleteGoal }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        style={({ pressed }) => {
+          return pressed ? { color: "#210644" } : {};
+        }}
+        android_ripple={{ color: "#210644" }}
+        onPress={deleteGoal}
+      >
+        <Text style={styles.goalText}>{itemData.item.text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
+});
+export default GoalItem;
